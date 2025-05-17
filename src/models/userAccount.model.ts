a7654c101d3f8bde14187f2a4b7d632399928cd9
Alex <alejandromontes393@gmail.com>
2025-05-17 10:35:41 -0600

    feat: create UserAccount model with necessary fields and associations

---
+++ src/models/userAccount.model.ts
@@ -0,0 +1,62 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../config/database.js'
+export const UserAccount = sequelize.define(
+  'UserAccount',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'User',
+        key: 'id',
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    external_user_id: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    platform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    access_token: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    refresh_token: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    creater_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    deleted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    expiry_date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: 'user_accounts',
+  },
+)
