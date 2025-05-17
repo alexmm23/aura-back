7b8ffc0573fb949fd5005aeba1aa6ca76debc51b
Alex <alejandromontes393@gmail.com>
2025-05-17 12:55:21 -0600

    feat: enhance Google authentication flow with state management and user account checks

--- src/routers/googleAuth.router.ts
+++ src/routers/googleAuth.router.ts
@@ -4,73 +4,77 @@ import { getGoogleAuthUrl, getTokensFromCode } from '@/services/googleAuth.servi
 import { UserAccount } from '@/models/userAccount.model'
 import { authenticateToken } from '@/middlewares/auth.middleware'
 import { UserAttributes } from '@/types/user.types'
+import { googleAuthMiddleware } from '@/middlewares/googleAuth.middleware'
+import { User } from '@/models/user.model'
 const router = Router()
 
 // Redireccionar a Google
 router.get('/auth/google', authenticateToken, (req: Request & { user?: UserAttributes }, res) => {
-  // Verifica si el usuario ya está autenticado
-  // if (req.user) {
-  //   res.status(400).send('Ya estás autenticado.')
-  //   return
-  // }
-  // // Verifica si el usuario ya tiene una cuenta de Google
-  // if (req.user) {
-  //   res.status(400).send('Ya tienes una cuenta de Google vinculada.')
-  //   return
-  // }
-  // Genera la URL de autenticación de Google
-  // Aquí deberías obtener el user_id desde tu sistema de autenticación
   const userId = req.user?.id
   if (!userId) {
     res.status(400).send('No se pudo obtener el ID de usuario.')
     return
   }
-  const url = getGoogleAuthUrl()
-  res.redirect(url)
+  const state = req.headers['authorization']?.split(' ')[1] as string
+  if (!state) {
+    res.status(400).send('No se pudo obtener el token de autorización.')
+    return
+  }
+  const url = getGoogleAuthUrl({ state })
+  res.json({ url })
 })
 
 // Callback de Google
 router.get(
   '/auth/google/callback',
+  googleAuthMiddleware,
   authenticateToken,
   async (req: Request & { user?: UserAttributes }, res: Response) => {
-    // Verifica si el usuario está autenticado
     if (!req.user) {
-      res.status(401).send('Unauthorized')
+      res.status(400).send('No se pudo obtener el ID de usuario.')
       return
     }
-    const { code } = req.query
-
-    if (!code || typeof code !== 'string') {
-      res.status(400).send('No code provided')
-      return
-    }
-
     try {
-      const tokens = await getTokensFromCode(code)
-
       // Aquí deberías obtener el user_id desde tu sistema de autenticación
       const userId = req.user.id
       // Verifica si el usuario ya tiene una cuenta de Google
       const existingAccount = await UserAccount.findOne({
         where: {
           user_id: userId,
-          provider: 'google',
+          platform: 'google',
         },
       })
       if (existingAccount) {
-        res.status(400).send('Ya tienes una cuenta de Google vinculada.')
+        res.redirect('http://localhost:8081/profile')
+        return
+      }
+      const { email, password } = await User.findOne({
+        where: {
+          id: userId,
+        },
+      })
+      const { code } = req.query
+      if (!code || typeof code !== 'string') {
+        res.status(400).send('No se proporcionó el código de autorización.')
+        return
+      }
+      // Intercambia el código por un token de acceso
+      const tokens = await getTokensFromCode(code)
+      if (!tokens || !tokens.access_token) {
+        res.status(400).send('No se pudo obtener el token de acceso.')
         return
       }
 
       await UserAccount.create({
         user_id: userId,
-        provider: 'google',
+        platform: 'google',
         access_token: tokens.access_token,
         refresh_token: tokens.refresh_token,
+        username: email,
+        password: password,
       })
 
-      res.send('Autenticación completada correctamente.')
+      res.redirect('http://localhost:8081/profile')
     } catch (error: any) {
       res.status(500).json({ error: error.message })
     }
--- src/services/googleAuth.service.ts
+++ src/services/googleAuth.service.ts
@@ -8,13 +8,20 @@ const oauth2Client = new google.auth.OAuth2(
   GOOGLE_CLIENT_SECRET,
   GOOGLE_REDIRECT_URI,
 )
-export const getGoogleAuthUrl = () => {
+export const getGoogleAuthUrl = ({ state }: { state: string }) => {
   const scopes = ['https://www.googleapis.com/auth/classroom.courses.readonly']
-  return oauth2Client.generateAuthUrl({
+  const url = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: scopes,
     prompt: 'consent',
+    state,
   })
+  return url
+  // return oauth2Client.generateAuthUrl({
+  //   access_type: 'offline',
+  //   scope: scopes,
+  //   prompt: 'consent',
+  // })
 }
 export const getTokensFromCode = async (code: string) => {
   const { tokens } = await oauth2Client.getToken(code)
